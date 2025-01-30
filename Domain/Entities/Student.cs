26356fb686bea1f062b0397a170832257d0d6f2a
Radahn <kwakuaffram@gmail.com>
2025-01-30 12:00:45 +0000

    Update Student.cs
    Added Navigation props for Student.cs

--- Domain/Entities/Student.cs
+++ Domain/Entities/Student.cs
@@ -19,4 +19,18 @@ public class Student : BaseEntity
     public decimal Cgpa { get; set; }
     
     // Navigation Props
+    public ICollection<Enrollment> Enrollments { get; set; }
+    public ICollection<Grade> Grades { get; set; }
+    public ICollection<Payment> Payments { get; set; }
+    public ICollection<Research> ResearchProjects { get; set; }
+    public ICollection<LibraryAccess> LibraryAccesses { get; set; }
+
+    public Student()
+    {
+        Enrollments = new List<Enrollment>();
+        Grades = new List<Grade>();
+        Payments = new List<Payment>();
+        ResearchProjects = new List<Research>();
+        LibraryAccesses = new List<LibraryAccess>();
+    }
 }
